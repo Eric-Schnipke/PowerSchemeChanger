a1919bb85755910375ad51bbf2d57022d8f19eea
Eric Schnipke <Eric-Schnipke@users.noreply.github.com>
2016-10-21 17:34:14 -0400

    Update and rename PowerSchemeChanger.ps1 to PowerSchemeChanger.psm1

--- PowerSchemeChanger.psm1
+++ PowerSchemeChanger.psm1
@@ -1,5 +1,5 @@
 ###############################################################################
-# File:        PowerSchemeChanger.ps1
+# File:        PowerSchemeChanger.psm1
 #
 # AUTHOR:      Eric Schnipke, 2016
 #
@@ -8,8 +8,14 @@
 # DESCRIPTION: Eases on-the-fly Windows power scheme changes within PowerShell.
 #              Aliases "power" cmd to "powercfg" and eases setting changing.
 #
-# USAGE:       Reference script within PowerShell profile to enable global
-#              usage.
+# USAGE:       1. Place this .psm1 file inside a directory named
+#                 "PowerSchemeChanger".
+#              2. Move this directory to
+#                 $home\Documents\WindowsPowerShell\Modules.
+#              3. In your PowerShell profile script, add
+#                 "Import-Module PowerSchemeChanger".
+#              4. From PowerShell command prompt, issue "power" command with
+#                 "p*", "h*", or "b*" arugments.
 ###############################################################################
 
 function power
